c754453fc5d9aada5059e69024ddfb8fd0b5c5e5
Orlando <yandelmartinez864@gmail.com>
2026-01-20 19:00:42 -0400

    Agregar archivos de proyecto.

---
+++ StudyReminder/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using StudyReminder.ViewModels;
+using StudyReminder.Views;
+using Xamarin.Forms;
+
+namespace StudyReminder
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ StudyReminder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using StudyReminder.ViewModels;
+using Xamarin.Forms;
+
+namespace StudyReminder.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
